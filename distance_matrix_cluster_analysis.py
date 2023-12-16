571e61aa03ebf80a013aeaa03b61800876f025b7
Artem <60553304+Artem7898@users.noreply.github.com>
2023-12-16 13:25:26 +0300

    Update distance_matrix_cluster_analysis.py

--- distance_matrix_cluster_analysis.py
+++ distance_matrix_cluster_analysis.py
@@ -1,14 +1,25 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Ваш код для получения данных и проведения кластерного анализа
-data = ...
+# Ваши данные
+data = {'Company A': [0, 1, 2, 3, 4],
+        'Company B': [1, 0, 3, 2, 5],
+        'Company C': [2, 3, 0, 1, 4],
+        'Company D': [3, 2, 1, 0, 5]}
 
-distance_matrix = ...  # Результат кластерного анализа
+distance_matrix = pd.DataFrame(data)
 
 sns.heatmap(distance_matrix, cmap='viridis', linewidths=.5)
 plt.title('Матрица расстояний в кластерном анализе')
 plt.show()
+
+
+
+
+
+
+
 
 
 
