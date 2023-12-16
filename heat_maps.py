7d33e8b5affecc9c4111aff79e9cd334f36fab7e
Artem <60553304+Artem7898@users.noreply.github.com>
2023-12-16 13:26:06 +0300

    Update heat_maps.py

--- heat_maps.py
+++ heat_maps.py
@@ -1,11 +1,24 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Ваш код для получения данных
-data = ...
+# Пример данных
+data = {'Variable1': [1, 2, 3, 4, 5],
+        'Variable2': [5, 4, 3, 2, 1],
+        'Variable3': [2, 3, 1, 5, 4],
+        'Variable4': [4, 1, 5, 2, 3]}
 
-correlation_matrix = data.corr()
+# Создаем DataFrame
+df = pd.DataFrame(data)
 
+# Вычисляем корреляционную матрицу
+correlation_matrix = df.corr()
+
+# Строим тепловую карту
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
 plt.title('Корреляционная тепловая карта')
 plt.show()
+
+
+
+
